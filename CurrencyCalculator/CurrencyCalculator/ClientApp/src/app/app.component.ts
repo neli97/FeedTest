a3ce9794a2c3c27a32a0288c7466bc9df17c97b9
neli97 <neli.zarkova@abv.bg>
2019-11-21 16:42:13 +0200

    write first page

--- CurrencyCalculator/CurrencyCalculator/ClientApp/src/app/app.component.ts
+++ CurrencyCalculator/CurrencyCalculator/ClientApp/src/app/app.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient  } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ClientApp';
-  //
+export class AppComponent implements OnInit {
+
+  latestPosts: Feed[] = [];
+
+  constructor(private http: HttpClient) { } 
+  
+  ngOnInit(){
+    this.http.get<Feed[]>('http://localhost:63330/api/rssfeeds').subscribe(result => {  
+      this.latestPosts = result;  
+    }, error => console.error(error));  
+  } 
+
 }
+
+interface Feed{
+  link: string;  
+  title: string;  
+  feedType: string;  
+  author: string;  
+  content: string;  
+  publishDate: string; 
+}
+
